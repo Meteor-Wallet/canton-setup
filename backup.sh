cd92243d722d8f10bbf219fed7e7ee9d508a5a89
Steve Kok <kokchoquan@gmail.com>
2026-02-25 12:28:10 +0800

    fix: create backup folder if not exist

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@ set -e
 
 . ./validator/.env
 
+mkdir -p ./backup
 docker exec -i splice-validator-postgres-splice-1 pg_dump -U cnadmin validator > ./backup/validator-"$(date -u +"%Y-%m-%dT%H:%M:%S%:z")".dump
 active_participant_db=$(docker exec splice-validator-participant-1 bash -c 'echo $CANTON_PARTICIPANT_POSTGRES_DB')
 docker exec splice-validator-postgres-splice-1 pg_dump -U cnadmin "${active_participant_db}" > "./backup/${active_participant_db}-$(date -u +"%Y-%m-%dT%H:%M:%S%:z").dump"
